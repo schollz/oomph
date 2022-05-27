c6958db29689bd3e20314bfb044a385334b3da7b
Zack Scholl <zack.scholl@gmail.com>
2022-05-26 21:16:55 -0700

    sweet

--- acid-pattern.lua
+++ acid-pattern.lua
@@ -14,6 +14,7 @@ local lattice_=require("lattice")
 s=require("sequins")
 ap_=include("lib/AcidPattern")
 local shift=false
+local pos={1,3}
 
 function init()
   ap=ap_:new()
@@ -50,8 +51,9 @@ function key(k,z)
     end
   else
     if k==1 then
-    elseif k==2 then
-    else
+    elseif k>1 and z==1 then 
+      local d=k*2-5
+      ap:set(pos[1],pos[2],d)
     end
   end
 end
@@ -64,25 +66,50 @@ function enc(k,d)
     end
   else
     if k==1 then
-    elseif k==2 then
-    else
+    elseif k>1 then
+      local k_=k==2 and 2 or 1
+      pos[k_]=pos[k_]+(k==2 and d or -d)
+      if pos[k_]>16 and k_==2 then 
+        pos[k_]=pos[k_]-16
+      end
+      if pos[k_]<1 and k_==2 then 
+        pos[k_]=pos[k_]+16
+      end
+      if pos[k_]>4 and k_==1 then 
+        pos[k_]=pos[k_]-4
+      end
+      if pos[k_]<1 and k_==1 then 
+        pos[k_]=pos[k_]+4
+      end
     end
   end
 end
 
 function redraw()
   screen.clear()
-  local x=1
-  local y=1
-  local sh=7
-  local sw=7
+  screen.level(15)
+  screen.blend_mode(0)
+  local x=2.5
+  local y=13
+  local sh=9
+  local sw=8
   for i=1,16 do
-    screen.text_center(x+sw*(i-1),y,i)
-    screen.text_center(x+sw*(i-1),y+sw*1,ap.note[i])
-    screen.text_center(x+sw*(i-1),y+sw*2,ap.octave[i])
-    screen.text_center(x+sw*(i-1),y+sw*3,ap.accent[i])
-    screen.text_center(x+sw*(i-1),y+sw*4,ap.duration[i])
+    screen.move(x+sw*(i-1),y)
+    screen.text_center(i)
+    screen.move(x+sw*(i-1),y+sh*1)
+    screen.text_center(ap.key_notes[ap.note[i]])
+    screen.move(x+sw*(i-1),y+sh*2)
+    screen.text_center(ap.key_octave[ap.octave[i]])
+    screen.move(x+sw*(i-1),y+sh*3)
+    screen.text_center(ap.key_accent[ap.accent[i]])
+    screen.move(x+sw*(i-1),y+sh*4)
+    screen.text_center(ap.key_punctuation[ap.punct[i]])
   end
+  screen.move(x+sw*(ap.current-1),y-5)
+  screen.text_center("^")
+  screen.blend_mode(1)
+  screen.rect(x+sw*(pos[2]-1)-4,y+sh*(pos[1]-1)+2,10,9)
+  screen.fill()
   screen.update()
 end
 
--- lib/AcidPattern.lua
+++ lib/AcidPattern.lua
@@ -2,7 +2,7 @@ local AP={}
 
 local PUNCUATION_NOTE=1
 local PUNCTUATION_REST=2
-local DURATION_HOLD=3
+local PUNCTUATION_TIE=3
 
 function AP:new(o)
   -- https://www.lua.org/pil/16.1.html
@@ -18,11 +18,12 @@ end
 
 function AP:init()
   -- https://acidpattern.bandcamp.com/album/july-acid-pattern-2014
+  self.current=1
   self.note_scale={33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50}
-  self.key_notes={"C","D","E"} --TODO fix this
+  self.key_notes={"A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab"}
   self.key_octave={"D","","U"}
   self.key_accent={"","A","S"}
-  self.key_duration={"*","o","-"}
+  self.key_punctuation={"@","o","-"}
   -- do initialize here
   self.note=    {1,1,7,4,4,1,1,7,4,4,1,1,7,4,4,1}
   self.octave=  {2,1,2,2,2,2,1,2,2,2,2,1,2,2,2,2}
@@ -34,8 +35,21 @@ function AP:init()
   self.step_s=s(self.step)
 end
 
+function AP:set(ind,pos,d)
+  local setters={"note","octave","accent","punct"}
+  local maxes={12,3,3,3}
+  print(setters[ind],pos)
+  self[setters[ind]][pos] = self[setters[ind]][pos] + d
+  if self[setters[ind]][pos]<1 then 
+    self[setters[ind]][pos] = self[setters[ind]][pos] + maxes[ind]
+  elseif self[setters[ind]][pos]>maxes[ind] then 
+    self[setters[ind]][pos] = self[setters[ind]][pos] - maxes[ind]
+  end
+end
+
 function AP:process()
   local i=self.step_s()
+  self.current=i
   if self.punct[i]==PUNCTUATION_REST then
     do return end
   end
